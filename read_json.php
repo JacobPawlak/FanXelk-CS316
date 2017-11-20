040d3339033debbe8c8d5f178533c26b0136fed5
Jacob Pawlak <jcpa229@cs.uky.edu>
2017-11-19 19:21:02 -0500

    updated file so that it reads in json files now

--- read_json.php
+++ read_json.php
@@ -2,7 +2,14 @@
 
 <?php
 
-	//echo "Hello World! \n";
-	
+
+	$sports_file = file_get_contents("Sports.json") or die ("That file does not exist in this directory");
+	$json_array = json_decode($sports_file, true);
+
+	var_dump($json_array);
+
+	//echo json_last_error();
+	echo;
+
 
 ?>
